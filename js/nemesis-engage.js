cef91fca8df8186d760009fe9433fe1912f6ac00
John <johntbrooks92@gmail.com>
2020-11-16 13:10:23 -0600

    music fixed for start up

--- js/nemesis-engage.js
+++ js/nemesis-engage.js
@@ -1,1 +1,39 @@
 "use strict";
+// Keystroke to start audio background
+var musicCode =[13]
+var inputCode=[]
+
+$('body').keyup(function(event) {
+    $(this).scrollTop(0);
+    inputCode.push(event.which);
+    console.log(inputCode);
+
+    if (inputCode.join(',') === musicCode.join(',')) {
+        $('audio#battleThemeSaiyans')[0].play();
+        $('#enterNotification').css({
+            'display': 'none'
+        });
+    }
+
+
+
+
+
+
+
+});
+//
+//
+//
+// alert("Click here to begin the battle!");
+
+
+
+document.getElementById("battleThemeSaiyans").volume = 0.35;
+
+// var music = new Audio();
+// function playMusic(file) {
+//     music.pause();
+//     music = new Audio(file);
+//     music.play();
+// }
