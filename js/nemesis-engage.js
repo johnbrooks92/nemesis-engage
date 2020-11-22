1a6ae3952bd532c12e9d20472468954ed816362b
John <johntbrooks92@gmail.com>
2020-11-22 00:13:31 -0600

    goku transformation functionality coming along

--- js/nemesis-engage.js
+++ js/nemesis-engage.js
@@ -45,6 +45,7 @@ var princeKi = 1200;
 //
 //
 
+// Functionality for Moves
 function kamehameha(){
     var hitChance = Math.round(Math.random()*10);
     if (hitChance >=2 && sonKi > 155) {
@@ -69,9 +70,44 @@ function kamehameha(){
 
 
 
+// Functionality for Transformations
+
+$(document).ready(function(){
+    //Goku Transformation Effects
+    $('#sonGokuSS').hide();
+    $('#sonGokuSS2').hide();
+    $('#sonGokuSS3').hide();
+    $('#sonGokuSSG').hide();
+    $('#sonGokuSSGSS').hide();
 
 
 
+    $('#transformGoku1').click(function(){
+        $('#sonGokuBase').hide();
+        $('audio#ssScreamGoku')[0].play();
+        $('#sonGokuSS').show();
+    });
+    $('#transformGoku2').click(function(){
+        $('#sonGokuSS').hide();
+        $('audio#ss2ScreamGoku')[0].play();
+        $('#sonGokuSS2').show();
+    });
+    $('#transformGoku3').click(function(){
+        $('#sonGokuSS2').hide();
+        $('audio#ss3ScreamGoku')[0].play();
+        $('#sonGokuSS3').show();
+    });
+    $('#transformGoku4').click(function(){
+        $('#sonGokuSS3').hide();
+        $('audio#ssGScreamGoku')[0].play();
+        $('#sonGokuSSG').show();
+    });
+    $('#transformGoku5').click(function(){
+        $('#sonGokuSSG').hide();
+        $('audio#ssGssScreamGoku')[0].play();
+        $('#sonGokuSSGSS').show();
+    });
+});
 
 
 
